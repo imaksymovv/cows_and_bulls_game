e9157b743ddd93188bc12facdc03c324cb5d465d
Ivan Maksymov <Ivan.Vasiliy.Maksymov@gmail.com>
2022-12-24 15:44:33 +0100

    Add substitute_creating.h and CowsAndBullsComputerPlayer.h files

---
+++ candb_final/substitute_creating.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "CowsAndBullsComputerHelper.h"
+
+unsigned char substitute_creating(CowsAndBullsComputerHelper r) {
+    unsigned char substitute = 0;
+    bool numbers_are_not_same = false;
+    for (size_t i = 0; i < 9; i++) {
+        substitute = i + 1;
+        for (size_t j = 0; j < 4; j++) {
+            if (substitute == r.computer[j]) {
+                numbers_are_not_same = false;
+                break;
+            }
+            else {
+                numbers_are_not_same = true;
+            }
+        }
+        if (numbers_are_not_same == true) {
+            break;
+        }
+    }
+    return substitute;
+}
